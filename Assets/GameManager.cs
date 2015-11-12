9d2e5a867a737d6dee546d5e3580682ad5505adb
김우찬 <tgboys@dragonflygame.com>
2015-11-12 14:22:40 +0900

    기능 1 시간에 따른 골드 올리기 보여줌.
    1번 버튼 업데이트

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,97 +1,87 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	private long Maintime;
+	//버튼 객체.
+	public Funtion01_MoneyIncrese mFuntion_Button01;
+	
+	//최상단 시간.
+	private DateTime Maintime;
+	private DateTime Maintime2;
+
+	public Text SystemTime;
+	public Text SpendTime;
+
+	public DateTime addedTime;
+
+	//[11/11/2015 tgboys] // 시간 비교 후 결과 값을 위한 구조체.
+	private enum DateComparisonResult
+	{
+		Earlier = -1,
+		Later = 1,
+		TheSame = 0
+	};
+	
+	//평션1 시간이 흐를 때마다 돈을 획득, 맥스값이 있음.
+
+	public Text totalgold;
+	public float NumberTotalGold = 0;
 
 
-	//평션1
-	public Button basic01;
-	public Text text01;
-	private float timer01;
-	public float Max01;
+
+	public float checkFlowTime; 
 
 	public Button basic02;
 	public Text text02;
 	private float timer02;
 	public float Max02;
-	
-	public Text BasicTime01_text;
+
 	private float BasicTime01;
-
-	public Text totalgold;
-	private float NumberTotalGold = 0;
-
+	
 	//펑션2
 
 	public Text Funtion2gold;
 	private float Funtion2Gold_number;
-	public Text funtion2_Time;
-	private float BasicTime02;
+
 
 	public Button funtion02_Button01;
 	public Text funtion02_Buttion01_text;
 	public float funtion02_button01_WaitTime;
 	public float funtion02_button01_Gold = 100;
-
+	public DateTime funtion02_Time;
 
 	public Button funtion02_Button02;
 	public Text funtion02_Buttion02_text;
-	//public float funtion02_button02_Gold;
+
 
 
 	// Use this for initialization
 	void Start () 
 	{
+		//[11/11/2015 tgboys] // 펑션 2를 위한 글로벌 타임 세팅과 펑션2의 시간 세팅. 추후에는 리스트로 만들어서 체크 해야함.
+		//Maintime = System.DateTime.Now;
+		//Debug.Log ("maintime is "+Maintime);
+
+		//펑션2가 눌러서 수확을 하면, 지금 시간기준으로 얼마나 기다려야 되는지 확인.
+		//여기서는 Start할때 넣어주지만, 나중엔 리스트에서 불렁오면 됨. 시작시에.
+		funtion02_Time = Maintime.AddMinutes (funtion02_button01_WaitTime); //30초 추가.
+		//Debug.Log ("mainTime is " + Maintime + " // funtion02_time is " + funtion02_Time);
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		//일반적인 시간의 흐름
-		//Debug.Log (Time.deltaTime);
+		Maintime2 = System.DateTime.Now;
 
-		//시간 관련 TEST
-		//Debug.Log("1 -> " + System.DateTime.UtcNow); //세계 협정시
-		//Debug.Log("2 -> " +System.DateTime.Now); //로컬 시
-
-		Maintime = System.DateTime.UtcNow.ToBinary ();
+		SystemTime.text = Maintime2.ToString ();
 
 
-		//여기는 1번 기능에 대한 내용.
-		BasicTime01 += Time.deltaTime;
-
-		if (timer01 <= Max01) 
-		{
-			timer01 += Time.deltaTime;
-		} 
-		else 
-		{
-			timer01 = Max01;
-		}
-
-		if (timer02 <= Max02) 
-		{
-			timer02 += Time.deltaTime;
-		} 
-		else 
-		{
-			timer02 = Max02;
-		}
-
-		text01.text = "Time:" + timer01.ToString ("N2");
-
-		text02.text = "Time:" + timer02.ToString ("N2");
-
-		BasicTime01_text.text = "Time:" + BasicTime01.ToString ("N0");
 
 		//여기는 2번 기능에 대한 내용.
-		//BasicTime02 = Time.deltaTime;
-
-		funtion02_button01_WaitTime -= Time.deltaTime;
 
 		if (funtion02_button01_WaitTime > 0) {
 
@@ -107,21 +97,29 @@ public class GameManager : MonoBehaviour {
 		}
 
 
+		DateComparisonResult comparison_Funtion02;
+
+		comparison_Funtion02 = (DateComparisonResult)funtion02_Time.CompareTo (Maintime); //Funtion2랑 현재시간이랑 비교.
+
+		//Debug.Log ("Comparision is " + comparison_Funtion02 +"....F2 Time is " + funtion02_Time + "main Time is " + Maintime );
+
+		if (comparison_Funtion02 > 0) {
+			
+			funtion02_Button01.interactable = false;
+			
+		}
+		
+		else if (comparison_Funtion02 < 0) {
+			
+			funtion02_Buttion01_text.text = "Ready";
+			
+			funtion02_Button01.interactable = true;
+		}
+
+
+
 	}
 
-	public void GetGoldButton01()
-	{
-		float a;
-		a = timer01;
-
-		NumberTotalGold += a;
-		totalgold.text = NumberTotalGold.ToString ("N2");
-		timer01 = 0;
-
-		Debug.Log (totalgold.text);
-		Debug.Log ("Clicked");
-
-	}
 
 	public void GetGoldButton02()
 	{
@@ -142,9 +140,9 @@ public class GameManager : MonoBehaviour {
 	{
 		Funtion2Gold_number += funtion02_button01_Gold;
 
-		BasicTime02 = 0;
+		//시간 찍기.
 
-		funtion02_button01_WaitTime = 5;
+		funtion02_Time = Maintime.AddMinutes (funtion02_button01_WaitTime);
 
 		Funtion2gold.text = Funtion2Gold_number.ToString ("N2");
 

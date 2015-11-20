3fb71e256f100cd576cb57dca1707c91f5524d5a
김우찬 <tgboys@dragonflygame.com>
2015-11-20 22:19:32 +0900

    타워씬 추가
    ㄱㄱ

---
+++ Assets/Scripts/ScrollbarSet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollbarSet : MonoBehaviour {
+
+    public Scrollbar mScrollbar;
+
+	// Use this for initialization
+	void Start () {
+        mScrollbar.value = 1.0f; //시작하면 맨 상단으로 간당!
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/UIEventManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIEventManager : MonoBehaviour {
+
+    public GameObject UI_MainPage;
+    public GameObject UI_ScrollPage;
+
+
+	// Use this for initialization
+	void Start () {
+
+        UI_MainPage.SetActive(true);
+        UI_ScrollPage.SetActive(false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void openScrollpage() 
+    {
+        UI_MainPage.SetActive(false);
+        UI_ScrollPage.SetActive(true);
+    }
+
+    public void closeScrollpage() 
+    {
+        UI_MainPage.SetActive(true);
+        UI_ScrollPage.SetActive(false);
+    }
+    
+}
